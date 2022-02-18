207058fe59a60bde0b7c4d311e82a962ea52e547
Will H <will.wkhu@gmail.com>
2022-02-17 23:55:01 -0800

    Update src asset directory, webpack.config file

--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,16 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
-      }
-    ]
-  }
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+     },
+    ],
+  },
 };
 
