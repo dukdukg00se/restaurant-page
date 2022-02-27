e76b3e48967e2d84d122cddc9840408e6fb40982
Will H <will.wkhu@gmail.com>
2022-02-26 23:28:36 -0800

    Refactor createHome function

--- src/modules/home.js
+++ src/modules/home.js
@@ -24,10 +24,8 @@ export default function createHome() {
   const logoImg = document.createElement('img');
   logoImg.id = 'logo-img';
   logoImg.src = restaurantLogo;
-
-
-
   logoImg.alt = 'restaurant logo';
+
   logoLink.appendChild(logoImg);
 
   const navLinks = document.createElement('ul');
@@ -162,17 +160,15 @@ export default function createHome() {
 
   }
 
-
-
   // add credit container and link to github
   const creditContainer = document.createElement('div');
   creditContainer.id = 'credits';
+
   const githubLink = document.createElement('a');
   githubLink.id = 'github-link';
   githubLink.href = 'https://github.com/dukdukg00se/restaurant-page';
   githubLink.textContent = 'Created by dukdukg00se 2021';
 
-  
   const githubImage = document.createElement('img');
   githubImage.src = githubLogo;
   githubImage.alt = '';
@@ -187,10 +183,8 @@ export default function createHome() {
     githubImage.src = githubLogo;
   });
 
-
   githubLink.append(githubImage);
   creditContainer.append(githubLink);
-
   footer.append(mediaContainer, creditContainer);
   content.append(header, main, footer);
 }
@@ -217,3 +211,152 @@ function setReview(elmnt, attrs, elmntContainer) {
   elmntContainer.append(elmnt, cite);
 }
 
+
+
+/************************************************** 
+Refactor above createHome function and break into more small, more manageable functions 
+***************************************************/
+
+// function createHeader() {
+//   const header = document.createElement('header');
+//   header.id = 'header';
+
+//   const logoLink = document.createElement('a');
+//   logoLink.id = 'logo-link';
+//   logoLink.href = '#content';
+
+//   const logoImg = document.createElement('img');
+//   logoImg.id = 'logo-img';
+//   logoImg.src = restaurantLogo;
+//   logoImg.alt = 'restaurant logo';
+
+//   const navLinks = document.createElement('ul');
+//   navLinks.classList.add('nav-links');
+
+//   // array of attributes for nav anchor elements
+//   let navAnchorAttrs = [
+//     {
+//       'id': 'story',
+//       'href': '#story',
+//       'textContent': 'Our Story'
+//     },
+//     {
+//       'id': 'menu',
+//       'href': '#menu',
+//       'textContent': 'Menu'
+//     },
+//     {
+//       'id': 'locations',
+//       'href': '#locations',
+//       'textContent': 'Locations & Hours'
+//     },
+//     {
+//       'id': 'contact',
+//       'href': '#contact',
+//       'textContent': 'Contact'
+//     }
+//   ];
+//   for (let i = 0; i < navAnchorAttrs.length; i++) {
+//     const navItem = document.createElement('li');
+//     navItem.classList.add('nav-item');
+
+//     const navAnchor = document.createElement('a');
+//     setAttributes(navAnchor, navAnchorAttrs[i]);
+    
+//     navItem.append(navAnchor);
+//     navLinks.append(navItem);
+//   }
+
+//   logoLink.appendChild(logoImg);
+//   header.append(logoLink, navLinks);
+// }
+
+// function createfooter() {
+//   const footer = document.createElement('footer');
+
+//     // add media container and media links
+//   const mediaContainer = document.createElement('div');
+//   mediaContainer.id = 'media-container';
+
+//   const anchorAttrs = [
+//     {
+//       'href': 'https://www.facebook.com/'
+//     },
+//     {
+//       'href': 'https://www.instagram.com/'
+//     },
+//     {
+//       'href': 'https://www.twitter.com/'
+//     },
+//     {
+//       'href': 'https://www.linkedin.com/'
+//     }
+//   ]
+//   const imageAttrs = [
+//     {
+//       'alt': 'facebook logo',
+//       'src': facebookLogo
+//     },
+//     {
+//       'alt': 'instagram logo',
+//       'src': instagramLogo
+//     },
+//       {
+//       'alt': 'twitter logo',
+//       'src': twitterLogo
+//     },
+//       {
+//       'alt': 'linkedin logo',
+//       'src': linkedinLogo
+//     },
+//   ]
+
+//   for (let i = 0; i < imageAttrs.length; i++) {
+//     const mediaLink = document.createElement('a');
+//     mediaLink.classList.add('media-link');
+//     mediaLink.target = '_blank';
+//     setAttributes(mediaLink, anchorAttrs[i]);
+
+//     const mediaImg = document.createElement('img');
+//     mediaImg.classList.add('media-img');
+//     setAttributes(mediaImg, imageAttrs[i]);
+
+//     mediaLink.appendChild(mediaImg);
+//     mediaContainer.append(mediaLink);
+//   }
+
+//   // add credit container and link to github
+//   const creditContainer = document.createElement('div');
+//   creditContainer.id = 'credits';
+
+//   const githubLink = document.createElement('a');
+//   githubLink.id = 'github-link';
+//   githubLink.href = 'https://github.com/dukdukg00se/restaurant-page';
+//   githubLink.textContent = 'Created by dukdukg00se 2021';
+
+//   const githubImage = document.createElement('img');
+//   githubImage.src = githubLogo;
+//   githubImage.alt = '';
+//   githubImage.id = 'github-logo';
+
+//   // wan't to change img bkgrnd color on hover
+//   // but can't change svg fill attr when using img tag to insert svg
+//   // change img src for similar effect
+//   githubLink.addEventListener('mouseenter', function() {
+//     githubImage.src = githubLogoWhite;
+//   });
+//   githubLink.addEventListener('mouseleave', function() {
+//     githubImage.src = githubLogo;
+//   });
+
+//   githubLink.append(githubImage);
+//   creditContainer.append(githubLink);
+//   footer.append(mediaContainer, creditContainer);
+// }
+
+
+// function setAttributes(elmnt, attrs) {
+//   for (let key in attrs) {
+//     key === 'textContent' ? elmnt.textContent = attrs.textContent : elmnt.setAttribute(key, attrs[key]);
+//   }
+// }
