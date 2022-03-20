25f99483c01c3e354ada5afc5bf0fc84c15bc189
Will H <will.wkhu@gmail.com>
2022-03-20 15:47:16 -0700

    Add address content, refactor setAttrs function

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { createHome } from './modules/home.js';
 import displayReviews from './modules/reviews.js';
 import displayStory from './modules/story.js';
 import displayMenu from './modules/menu.js';
-import displayLocHrs from './modules/location.js';
+import displayLocatn from './modules/location.js';
 
 createHome();
 displayReviews();
@@ -22,21 +22,23 @@ const navItems = document.querySelectorAll('li');
 
 navItems.forEach((item) => {
   item.addEventListener('click', (e) => {
+    // console.log(e.target.id);
 
     if (e.target.id === 'home') {
       main.removeChild(main.firstElementChild);
       displayReviews();
     } else if (e.target.id === 'story') {
+      // console.log(main.firstElementChild);
       main.removeChild(main.firstElementChild);
       displayStory();
-      // console.log('story');
+      
     } else if (e.target.id === 'menu') {
       main.removeChild(main.firstElementChild);
       displayMenu();
       // console.log('menu');
     } else if (e.target.id === 'location-hours') {
       main.removeChild(main.firstElementChild);
-      displayLocHrs();
+      displayLocatn();
     }
 
 
--- src/modules/location.js
+++ src/modules/location.js
@@ -1,17 +1,80 @@
-const displayLocHrs = () => {
+import '../styles/location.css';
+
+const anchorAttrs = [
+  {
+    href: 'https://goo.gl/maps/WLfnuS4vb1fdn7eM6',
+    textContent: '1420 Baker Street, Suite B, ',
+    span: {
+      id: 'city-span',
+      textContent: 'Costa Mesa, CA 92626'
+    }
+  },
+  {
+    href: 'tel:714-708-2143',
+    textContent: '714.708.2143'
+  },
+  {
+    href: 'mailto:info@michelangiolesca.com',
+    textContent: 'info@michelangiolesca.com'
+  },
+];
+
+const displayLocatn = () => {
   const main = document.querySelector('main');
 
-  const locHrsContainer = document.createElement('div');
-  locHrsContainer.id = 'lochrs-container';
+  const locationContainer = document.createElement('div');
+  locationContainer.id = 'lochrs-container';
   // main.append(locHrsContainer);
 
   const title = document.createElement('h1');
   title.id = 'lochrs-heading';
   title.textContent = 'Location & Hours';
-  locHrsContainer.append(title);
+  locationContainer.append(title);
 
-  main.append(locHrsContainer);
+  const contactContainer = document.createElement('address');
+  contactContainer.id = 'location';
+
+
+  for (let i = 0; i < anchorAttrs.length; i++) {
+    const anchor = document.createElement('a');
+    anchor.classList.add('contact');
+    setAttrs(anchor, anchorAttrs[i]);
+
+    contactContainer.append(anchor)
+  }
+
+  
+
+  locationContainer.append(contactContainer);
+  main.append(locationContainer);
 
 }
 
-export default displayLocHrs;
+function setAttrs(elmnt, obj) {
+
+  for (let key in obj) {
+    if (key === 'textContent') {
+      elmnt.textContent = obj[key];
+    } else if (key === 'span') {
+      const elmntSpan = document.createElement('span');
+      // properties in span obj
+      // console.log(obj[key]);
+      for (let subKey in obj[key]) {
+        console.log(subKey);
+        // console.log(obj.key);
+
+        setAttrs(elmntSpan, obj[key]);
+        
+        elmnt.append(elmntSpan);
+
+      }
+    } else {
+      elmnt.setAttribute(key, obj[key]);
+    }
+  }
+
+}
+
+
+
+export default displayLocatn;
