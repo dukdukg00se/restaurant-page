624cb09d820c87282e77145e299d369c096acbf1
Will H <will.wkhu@gmail.com>
2022-03-26 23:36:09 -0700

    Move anchorAttrs from global to displayLocatn()

--- src/modules/location.js
+++ src/modules/location.js
@@ -1,26 +1,6 @@
 import {createMnSubContnr, setElmnt} from './home.js';
 import '../styles/location.css';
 
-// Array holds the attributes and text for address content
-let anchorAttrs = [
-  {
-    href: 'https://goo.gl/maps/WLfnuS4vb1fdn7eM6',
-    textContent: '1420 Baker Street, Suite B, ',
-    span: {
-      id: 'city-span',
-      textContent: 'Costa Mesa, CA 92626'
-    }
-  },
-  {
-    href: 'tel:714-708-2143',
-    textContent: '714.708.2143'
-  },
-  {
-    href: 'mailto:info@michelangiolesca.com',
-    textContent: 'info@michelangiolesca.com'
-  },
-];
-
 // Function appends "Location & Hours" content to main element
 const displayLocatn = () => {
   const main = document.querySelector('main');
@@ -35,6 +15,26 @@ const displayLocatn = () => {
 
   const contactContnr = document.createElement('address');
   contactContnr.id = 'location';
+
+  // Array holds the attributes and text for address content
+  let anchorAttrs = [
+    {
+      href: 'https://goo.gl/maps/WLfnuS4vb1fdn7eM6',
+      textContent: '1420 Baker Street, Suite B, ',
+      span: {
+        id: 'city-span',
+        textContent: 'Costa Mesa, CA 92626'
+      }
+    },
+    {
+      href: 'tel:714-708-2143',
+      textContent: '714.708.2143'
+    },
+    {
+      href: 'mailto:info@michelangiolesca.com',
+      textContent: 'info@michelangiolesca.com'
+    },
+  ];
 
   anchorAttrs.forEach(anchorObj => {
     const anchor = document.createElement('a');
