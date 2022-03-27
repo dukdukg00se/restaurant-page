dea89b28f77025c89293766da92742c8951740d2
Will H <will.wkhu@gmail.com>
2022-03-26 23:45:48 -0700

    clean code

--- src/modules/location.js
+++ src/modules/location.js
@@ -58,6 +58,7 @@ const displayLocatn = () => {
   hoursContnr.append(hoursPrgph);
 
   const subContnr = document.querySelector('#location-contnr');
+  
   subContnr.append(contactContnr, hoursContnr);
 }
 
