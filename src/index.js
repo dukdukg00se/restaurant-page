a695946e0c664ffcc94849b60008b4670fdddf96
Will H <will.wkhu@gmail.com>
2022-03-28 15:13:20 -0700

    Add comments, refactor nav item event listener

--- src/index.js
+++ src/index.js
@@ -4,41 +4,41 @@ import displayStory from './modules/story.js';
 import displayMenu from './modules/menu.js';
 import displayLocatn from './modules/location.js';
 
-
+// Show base web page and display restaurant reviews
+// This is the current home page
 createHome();
 displayRevws();
 
 const main = document.querySelector('main');
 
+// On logo click, display home page
 const home = document.querySelector('#logo');
 home.addEventListener('click', () => {
   main.removeChild(main.firstElementChild);
   displayRevws();
 })
 
+// Add event listeners on nav elements for page navigation
+// Replace if else w/ switch statement
 const navItems = document.querySelectorAll('li');
 navItems.forEach((item) => {
   item.addEventListener('click', (e) => {
-
-    if (e.target.id === 'home') {
-      main.removeChild(main.firstElementChild);
-      displayRevws();
-    } else if (e.target.id === 'story') {
-
-      main.removeChild(main.firstElementChild);
-      displayStory();
-      
-    } else if (e.target.id === 'menu') {
-      main.removeChild(main.firstElementChild);
-      displayMenu();
-
-    } else if (e.target.id === 'location-hours') {
-      main.removeChild(main.firstElementChild);
-      displayLocatn();
+    switch(e.target.id) {
+      case 'home':
+        main.removeChild(main.firstElementChild);
+        displayRevws();
+        break;
+      case 'story':
+        main.removeChild(main.firstElementChild);
+        displayStory();
+        break;
+      case 'menu':
+        main.removeChild(main.firstElementChild);
+        displayMenu();
+        break;
+      case 'location-hours':
+        main.removeChild(main.firstElementChild);
+        displayLocatn();
     }
-
   });
 });
-
-
-
